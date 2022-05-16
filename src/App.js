2be197b92884f4442467a39fb91332dff4d6b008
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-16 14:19:24 -0700

    wrote useEffect and functions for app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ import './App.css';
 function App() {
   const [pokemon, setPokemon] = useState([]);
   const [query, setQuery] = useState('');
+
+  async function load() {
+    const { data: { results } } = await getPokemon(query);
+    setPokemon(results);
+  }
+
+  useEffect(() => {
+    load();
+  }, []); //eslint-disable-line
+
+  async function handleSubmit(e) {
+    e.preventDefault;
+    load();
+  }
   return (
     <div className="App">
       
